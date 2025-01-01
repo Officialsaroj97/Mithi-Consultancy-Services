3d370d7f075808699899dc8757a6eb3108824297
Saroj Kumar Yadav <officialsaroj97@gmail.com>
2025-01-01 18:32:28 +0530

    add all

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -3,6 +3,12 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "./SkillsSection.css";
 
+// Import images from src/assets
+import mernImage from "../assets/mern.png";
+import appImage from "../assets/app.png";
+import digitalImage from "../assets/digital.png";
+import profileImage from "../assets/profile.png";
+
 const SkillsSection = () => {
   useEffect(() => {
     AOS.init({ duration: 1200 });
@@ -12,22 +18,22 @@ const SkillsSection = () => {
     {
       id: 1,
       title: "MERN Stack",
-      imageSrc: "../src/assets/mern.png",
+      imageSrc: mernImage, // Imported image
     },
     {
       id: 2,
       title: "Frontend Development",
-      imageSrc: "../src/assets/frontend.png",
+      imageSrc: appImage, // Imported image
     },
     {
       id: 3,
       title: "Backend Development",
-      imageSrc: "../src/assets/backend.png",
+      imageSrc: digitalImage, // Imported image
     },
     {
       id: 4,
       title: "Database Management",
-      imageSrc: "../src/assets/database.png",
+      imageSrc: profileImage, // Imported image
     },
   ];
 
